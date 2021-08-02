b6ed1bcd38027a09aa626dab174e852485c7191a
Wietse de Vries <wietsedvries@gmail.com>
2021-08-02 17:03:39 +0200

    add $CWD/bin to PATH

--- coman/env.py
+++ coman/env.py
@@ -488,16 +488,18 @@ def env_init(conda: Conda, spec: Specification, force: bool):
 
 
 def env_shell_hook(conda: Conda, quiet: bool, shell_type: str):
+    path_cmd = f'export PATH="{os.path.join(os.getcwd(), "bin")}:$PATH"'
+
     # Currently only works with conda or micromamba
     if not conda.is_micromamba():
         exe = conda.exe if conda.is_conda(standalone=False) else conda_exe()
         if exe:
             if not quiet:
                 print("You can deactivate the environment with `conda deactivate`", file=sys.stderr)
-            print(f"eval \"$('{exe}' shell.{shell_type} hook)\" && conda activate \"{conda.env.prefix}\"")
+            print(f"eval \"$('{exe}' shell.{shell_type} hook)\" && conda activate \"{conda.env.prefix}\"; {path_cmd}")
             exit(0)
 
     if not quiet:
         print("You can deactivate the environment with `micromamba deactivate`", file=sys.stderr)
     exe = conda.exe if conda.is_micromamba() else micromamba_exe()
-    print(f"eval \"$('{exe}' shell hook -s {shell_type})\" && micromamba activate \"{conda.env.prefix}\"")
+    print(f"eval \"$('{exe}' shell hook -s {shell_type})\" && micromamba activate \"{conda.env.prefix}\"; {path_cmd}")
